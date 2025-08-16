6db934ef0bdba2b3366d39baefe0a800f5d4e259
Rani Abu Sultan <raniabusultan@gmail.com>
2025-08-16 15:16:11 +0300

    process.env.PORT

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
-// thmanyah-api/src/main.ts
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   app.enableCors();
 
-  await app.listen(3001); // Let's use 3001 to avoid port conflicts
+  // Use the PORT from the environment, or 3001 if it's not set
+  const port = process.env.PORT || 3001;
+
+  await app.listen(port);
 }
 bootstrap();
