48fc815da2960def4f8a565f1df4c94234747644
Rani Abu Sultan <raniabusultan@gmail.com>
2025-08-16 09:54:24 +0300

    fix(backend): Configure package for production deployment

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+// thmanyah-api/src/main.ts
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors();
+
+  await app.listen(3001); // Let's use 3001 to avoid port conflicts
 }
 bootstrap();
